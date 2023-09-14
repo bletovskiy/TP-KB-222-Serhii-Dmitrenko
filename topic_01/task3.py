e27a027ba96dfd5db47ccdb6598c754e7f523fe5
Serhii <bletovskiy@gmail.com>
2023-09-14 14:44:46 +0300

    Discriminant function implementation

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,7 +1,11 @@
-a = float(input("Enter the coeficient a: "))
-b = float(input("Enter the coeficient b: "))
-c = float(input("Enter the coeficient c: "))
+def calculate_discriminant(a, b, c):
+    D = b**2 - 4*a*c
+    return D
 
-D = b**2 - 4*a*c
+a = float(input("Enter the coefficient a: "))
+b = float(input("Enter the coefficient b: "))
+c = float(input("Enter the coefficient c: "))
 
-print(f"The discriminant (D) is: {D}")
+discriminant = calculate_discriminant(a, b, c)
+
+print(f"The discriminant (D) is: {discriminant}")
