ed1a5872fae1266fa80da414768c7f02090aa722
Serhii Dmitrenko <51282094+bletovskiy@users.noreply.github.com>
2023-10-04 00:47:45 +0300

    Add lab_01 solution and empty lpr for lab_01

--- Lab_01/lab1.py
+++ Lab_01/lab1.py
@@ -1,93 +1,98 @@
-## List [Item1, Item2, Item3]
-## Item {"name":"Jon", "phone":"0631234567"}
+import random
 
-# already sorted list
 list = [
-    {"name":"Bob", "phone":"0631234567", "email":"bob@example.com", "age":"19"},
-    {"name":"Emma", "phone":"0631234567", "email":"emma@example.com", "age":"19"},
-    {"name":"Jon",  "phone":"0631234567", "email":"jon@example.com", "age":"21"},
-    {"name":"Zak",  "phone":"0631234567", "email":"zak@example.com", "age":"18"}
+    {"id": "00001", "name": "Bob", "phone": "0631234567", "age": "20", "email": "bob@example.com"},
+    {"id": "00002", "name": "Emma", "phone": "0631234568", "age": "22", "email": "emma@example.com"},
+    {"id": "00003", "name": "Jon", "phone": "0631234569", "age": "21", "email": "jon@example.com"},
+    {"id": "00004", "name": "Zak", "phone": "0631234570", "age": "19", "email": "zak@example.com"}
 ]
 
 def printAllList():
     for elem in list:
-        StrForPrint = "  Student name: " +elem["name"] + "  Phone Number: " + elem["phone"] + "  Email: " + elem["email"] + "  Age: " + elem["age"]
+        StrForPrint = "  Student ID: "+ elem["id"] +"  Student name: " +elem["name"] + "  Phone Number: " + elem["phone"] + "  Age: " + elem["age"] + "  Email: " + elem["email"]
         print(StrForPrint)
     return
 
+def generateUniqueId():
+    while True:
+        unique_id = str(random.randint(10000, 99999))
+
+        if not any(elem["id"] == unique_id for elem in list):
+            return unique_id
+
 def addNewElement():
-    name = input("Pease enter student name: ")
+    name = input("Please enter student name: ")
     phone = input("Please enter student phone: ")
-    email = input("Pease enter student email: ")
     age = input("Please enter student age: ")
-    newItem = {"name": name, "phone": phone, "email": email, "age": age}
-    insertPosition = 0
-    for item in list:
-        if name > item["name"]:
-            insertPosition += 1
-        else:
-            break
-    list.insert(insertPosition, newItem)
+    email = input("Please enter student email: ")
+    
+    unique_id = generateUniqueId()
+    
+    newItem = {"id": unique_id, "name": name, "phone": phone, "age": age, "email": email}
+    
+    list.append(newItem)
     print("New element has been added")
-    return
 
 def deleteElement():
-    name = input("Please enter name to be delated: ")
-    deletePosition = -1
-    for item in list:
-        if name == item["name"]:
-            deletePosition = list.index(item)
-            break
-    if deletePosition == -1:
-        print("Element was not found")
-    else:
-        print("Dele position " + str(deletePosition))
-        del list[deletePosition]
-    return
+    id_to_delete = input("Please enter the ID of the student to be deleted: ")
+    found = False
 
+    for elem in list:
+        if elem.get("id") == id_to_delete:
+            list.remove(elem)
+            print("Student with ID '{}' has been deleted.".format(id_to_delete))
+            found = True
+            break
+
+    if not found:
+        print("Student with ID '{}' was not found.".format(id_to_delete))
 
 def updateElement():
-    name = input("Please enter name to be updated: ")
+    id_to_update = input("Please enter the ID of the student to be updated: ")
+    found = False
+
     for elem in list:
-        if elem["name"] == name:
-            print(elem["name"] + " found. Please write the information")
+        if elem.get("id") == id_to_update:
+            print("Student found. Enter updated information:")
             new_name = input("New name: ")
-            new_phoneNumber = input("New phone number: ")
-            new_email = input("New email: ")
+            new_phone = input("New phone: ")
             new_age = input("New age: ")
+            new_mail = input("New Email: ")
 
             elem["name"] = new_name
-            elem["phone"] = new_phoneNumber
-            elem["email"] = new_email
+            elem["phone"] = new_phone
             elem["age"] = new_age
+            elem["email"] = new_mail
 
-            print("Update successfully")
-            return
-    print("Not found")
+            print("Student information updated.")
+            found = True
+            break
+
+    if not found:
+        print("Student with ID '{}' was not found.".format(id_to_update))
 
 def main():
     while True:
-        chouse = input("Please specify the action [ C create, U update, D delete, P print,  X exit ] ")
-        match chouse:
-            case "C" | "c":
-                print("New element will be created:")
-                addNewElement()
-                printAllList()
-            case "U" | "u":
-                print("Existing element will be updated")
-                updateElement()
-                printAllList()
-            case "D" | "d":
-                print("Element will be deleted")
-                deleteElement()
-            case "P" | "p":
-                print("List will be printed")
-                printAllList()
-            case "X" | "x":
-                print("Exit()")
-                break
-            case _:
-                print("Wrong chouse")
-
+        choice = input("Please specify the action [C create, U update, D delete, P print, X exit]: ")
+        
+        if choice in ["C", "c"]:
+            print("New element will be created:")
+            addNewElement()
+            printAllList()
+        elif choice in ["U", "u"]:
+            print("Existing element will be updated")
+            updateElement()
+            printAllList()
+        elif choice in ["D", "d"]:
+            print("Element will be deleted")
+            deleteElement()
+            printAllList()
+        elif choice in ["P", "p"]:
+            print("List will be printed")
+            printAllList()
+        elif choice in ["X", "x"]:
+            break
+        else:
+            print("Wrong choice")
 
 main()
