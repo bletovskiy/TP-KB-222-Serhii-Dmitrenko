177a21644064302ffef944fa850f86091de03030
Serhii Dmitrenko <51282094+bletovskiy@users.noreply.github.com>
2023-10-09 23:32:35 +0300

    update task1

--- topic_04/task1.py
+++ topic_04/task1.py
@@ -1,7 +1,7 @@
 def add(a, b):
     return a + b
 
-def substract(a, b):
+def subtract(a, b):
     return a - b
 
 def multiply(a, b):
@@ -9,49 +9,47 @@ def multiply(a, b):
 
 def divide(a, b):
     try:
-        if b == 0:
-            raise ZeroDivisionError("Division by zero is not allowed")
-        return a / b
-    except ZeroDivisionError as exc:
-        return str(exc)
-
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        return "Division by zero is not allowed"
+    
 def getInt(prompt):
     while True:
         try:
             num = int(input(prompt))
             return num
         except ValueError:
-            print("Invalid input. Please enter an integer.")
+            print("Invalid input. Please enter a valid integer.")
+
+def getOperation(valid_operations):
+    while True:
+        operation = input("Enter operation: ")
+        if operation in valid_operations:
+            return operation
+        else:
+            print("Invalid operation. Please enter a valid operation.")
+
+valid_operations = ['+', '-', '*', '/']
 
 while True:
-    print(f"Add = '+'")
-    print(f"Subtract = '-'")
-    print(f"Multiply = '*'")
-    print(f"Divide = '/'")
-    print(f"Exit = 'Q' or 'q'")
-    operation = input("Enter operation: ")
+    print("Add = '+' \nSubtract = '-'\nMultiply = '*'\nDivide = '/'\nExit = 'Q' or 'q'")
+    operation = input("Enter operation : ")
 
-    if operation == "Q" or "q":
+    if operation == "Q" or operation == "q":
         break
-
-    a = getInt("Enter number 'a': ")
-    b = getInt("Enter number 'b': ")
-
-    try:
-        result = None
-        if operation == "+":
-            result = add(a, b)
-        elif operation == "-":
-            result = substract(a, b)
-        elif operation == "*":
-            result = multiply(a, b)
-        elif operation == "/":
-            result = divide(a, b)
-        else:
-            print("Invalid operation")
-
-        if result is not None:
-            print(f"Result: {result}\n")
-
-    except (ValueError, TypeError) as exc:
-        print(f"Error: {exc}")
+    elif operation in valid_operations:
+        a = getInt("Enter number 'a': ")
+        b = getInt("Enter number 'b': ")
+        
+        match operation:
+            case "+":
+                print(f"Result: {add(a, b)}\n")
+            case "-":
+                print(f"Result: {subtract(a, b)}\n")
+            case "*":
+                print(f"Result: {multiply(a, b)}\n")
+            case "/":
+                print(f"Result: {divide(a, b)}\n")
+    else:
+        print("Invalid operation. Please enter +, -, *, or /.\n")
