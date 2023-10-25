4a0034f52bd5aa703e5beccc437e722ecfc19db9
Serhii Dmitrenko <51282094+bletovskiy@users.noreply.github.com>
2023-10-25 12:31:24 +0300

    add solution for task2

--- topic_06/task2.py
+++ topic_06/task2.py
@@ -2,11 +2,18 @@ getName = lambda student: student['Name']
 getMark = lambda student: student['Mark']
 
 students = []
-with open('students.txt', 'r') as file:
+with open("student.txt", "r") as file:
     for line in file:
         parts = line.strip().split(',')
         if len(parts) == 2:
             name, mark = parts
             students.append({'Name': name, 'Mark': int(mark)})
-
+    
 sortName = sorted(students, key=getName)
+sortMark = sorted(students, key=getMark)
+
+for student in sortName:
+    print(f"{student['Name']}, {student['Mark']}")
+
+for student in sortMark:
+    print(f"{student['Name']}, {student['Mark']}")
